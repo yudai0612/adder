3310b1319915d2183155278b5eb245e2c545e033
yudai0612 <sqrt.374544@gmail.com>
2020-08-23 13:57:08 +0900

    fix: 不要な型変換の記述を削除

--- work/app/HalfAdder.php
+++ work/app/HalfAdder.php
@@ -2,10 +2,10 @@
 
 class HalfAdder {
     static function sum($in1, $in2) {
-        return (int)($in1 ^ $in2);
+        return $in1 ^ $in2;
     }
 
-    static function carry($in1, $in2):int {
-        return (int)($in1 && $in2);
+    static function carry($in1, $in2) {
+        return $in1 && $in2;
     }
 }
