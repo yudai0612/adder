fe4e6b712eb14bb9f9ddaa92b333d971f77122be
yudai0612 <sqrt.374544@gmail.com>
2020-08-23 20:02:26 +0900

    add: スタイルを調整

--- work/web/index.php
+++ work/web/index.php
@@ -1,40 +1,51 @@
 <?php
     require_once('../app/Adder.php');
-    const MAX_BIT = 4;
+    const MAX_BIT = 8;
     $x = $_REQUEST['former'];
     $y = $_REQUEST['latter'];
+
+    $adders = [];
+    $adders[0] = new HalfAdder();
+    for($i=1; $i<$_REQUEST['bit']; $i++) {
+        $adders[$i] = new FullAdder();
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic" rel="stylesheet">
+        <link rel="stylesheet" href="asset/css/style.css">
         <title>加算器作り</title>
     </head>
     <body>
-        <!-- 入力のbit数を貰うためのselectタグ -->
-        <select name="bit">
-            <?php for($i=1; $i<=MAX_BIT; $i++) : ?>
-                <option value="<?=$i?>"><?= $i ?></option>
-            <?php endfor; ?>
-        <select>
+        <div class="container">
+            <div> <!-- 入力のbit数を貰うためのselectタグ -->
+                <div class="label">入力bit数</div>
+                <select name="bit">
+                    <?php for($i=1; $i<=MAX_BIT; $i++) : ?>
+                        <option value="<?=$i?>"><?= $i ?></option>
+                    <?php endfor; ?>
+                <select>
+                    </div>
 
-        <!-- 選択したbit数の数だけチェックボックスを表示 -->
-        <?php 
-            $adders = [];
-            $adders[0] = new HalfAdder();
-            for($i=1; $i<$_REQUEST['bit']; $i++) {
-                $adders[$i] = new FullAdder();
-            }
-        ?>
-        <form method="post">
-            <div id="ckbxWrap0"></div>
-            <div id="ckbxWrap1"></div>
-            <input type="hidden" name="bit" value="0">
-            <input type="submit" value="submit">
-        </form>
-        
-        <p><?php var_dump( Adder::exec($x, $y) ); ?></p>
+            <p> <!-- 選択したbit数の数だけチェックボックスを表示 -->
+                <form method="post">
+                    <div>
+                        <div class="label">計算開始ボタン</div>
+                        <input type="submit" value="RUN">
+                    </div>
+                    <div>
+                        <div id="digit"><?php for($i=MAX_BIT-1; $i>=0; $i--){echo "<span class=\"n\">$i</span>";} ?></div>
+                        <div id="ckbxWrap0"></div>
+                        <div id="ckbxWrap1"></div>
+                        <div class="hissan">+)</div>
+                        <input type="hidden" name="bit" value="0">
+                    </div>
+                </form>
+            </p>
+        </div>
 
         <script src="asset/js/main.js"></script>
 
@@ -45,11 +56,12 @@
             <script>
                 var canvas = document.getElementById("canvas");
                 if (canvas.getContext){
-                    var ctx = canvas.getContext('2d');\n
+                    var ctx = canvas.getContext('2d');
+                    ctx.fillStyle = 'gray';\n
             HEAD;
 
             for($i=0; $i<$_REQUEST['bit']; $i++) {
-                $adders[$i]->show(200, 100+(100*$i));
+                $adders[$i]->show(200, 30+(100*$i));
             }
 
             echo <<<FOOT
